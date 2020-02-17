7e37588ae26c598fd97321c66333202c767b35bd
Fabiana Araujo <fabiana2611@hotmail.com>
2020-02-17 21:28:11 +0000

    adding injectble root

--- src/app/modules/contribuicao/contribuicao.service.ts
+++ src/app/modules/contribuicao/contribuicao.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { OpenDataBrService } from '../../core/http/open-data-br.service';
 
-@Injectable() 
+@Injectable({providedIn: 'root'}) 
 export class ContribuicaoService {
     
     constructor(private openDataRequest: OpenDataBrService){ }
